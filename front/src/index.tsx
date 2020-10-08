e21077663aeb82027dcb6956ce981c0b39e39ed0
kenwria1 <archie_william.kenwright@roche.com>
2020-10-08 10:38:08 +0200

    inspect redux

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import {reducers } from './reducer';
 
 
-const store = createStore(reducers);
+const store = createStore(reducers,window['__REDUX_DEVTOOLS_EXTENSION__'] && window['__REDUX_DEVTOOLS_EXTENSION__']());
 
 ReactDOM.render(
   <Provider store={store}>
