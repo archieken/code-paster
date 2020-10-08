6131929f00af9bc40d68b272a59561b8d7b2b8e0
kenwria1 <archie_william.kenwright@roche.com>
2020-10-08 10:15:30 +0200

    first

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { StylesProvider } from '@material-ui/styles';
 import App from './app';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import {reducers } from './reducer';
+
+
+const store = createStore(reducers);
 
 ReactDOM.render(
+  <Provider store={store}>
   <>
     <StylesProvider injectFirst>
       <App />
     </StylesProvider>
   </>,
+  </Provider>,
   document.getElementById('root')
 );
